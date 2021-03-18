39248745b1d7ed1a15780480ac3e47af23c49a21
elik <elik@example.com>
2021-03-18 11:37:25 +0200

    Load files

---
+++ ResourcesWPF/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ResourcesWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow dlg = new MainWindow();
+            dlg.ShowDialog();
+        }
+
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    ResourcesWithCodeBehind dlg = new ResourcesWithCodeBehind();
+        //    dlg.ShowDialog();
+        //}
+    }
+}
---
+++ ResourcesWPF/ResourcesWithCodeBehind.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ResourcesWPF
+{
+    /// <summary>
+    /// Interaction logic for ResourcesWithCodeBehind.xaml
+    /// </summary>
+    public partial class ResourcesWithCodeBehind : Window
+    {
+        public ResourcesWithCodeBehind()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            this.lbResult.Items.Clear();
+            //  Пошук локального ресурса блока pnlMain
+            this.lbResult.Items.Add(this.pnlMain.FindResource("strPnl").ToString());
+            //  Пошук ресурса Windows
+            this.lbResult.Items.Add(this.FindResource("strWindow").ToString());
+            //  Пошук глобального ресурса який доступний у всьому проекті
+            this.lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
+        }
+    }
+}
